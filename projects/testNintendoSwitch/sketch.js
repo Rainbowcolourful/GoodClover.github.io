26d508b361bbe113d9545ae23d6c46af26ae3234
GoodClover <oliversimmo@gmail.com>
2019-04-19 15:26:01 +0100

    Move test

--- projects/testNintendoSwitch/sketch.js
+++ projects/testNintendoSwitch/sketch.js
@@ -1,15 +1,32 @@
 function setup(){
     createCanvas(600,600)
+    L_UP = 38
+    L_DOWN = 40
+    L_LEFT = 37
+    L_RIGHT = 39
+    pos = createVector(width/2, height/2)
 }
 
 function draw() {
-    //if (keyIsPressed(38)) {
-    //    fs = fullscreen()
-    //    fullscreen(!fs)
-    //}
-
     background(150)
+
+    if (keyIsPressed(L_UP)) {
+        pos.y -= 1
+    } else if (keyIsPressed(L_DOWN)) {
+        pos.y += 1
+    }
+    if (keyIsPressed(L_LEFT)) {
+        pos.x -= 1
+    } else if (keyIsPressed(L_RIGHT)) {
+        pos.x += 1
+    }
+
+
+    noFill()
     textSize(40)
     textAlign(CENTER)
+
     text(keyCode, width/2, height/2)
+
+    point(pos.x, pos.y)
 }
