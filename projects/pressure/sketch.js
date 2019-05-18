97c4758519f0387c4c0f1a6676c2020a74e2ed3f
GoodClover <oliversimmo@gmail.com>
2019-05-18 10:03:59 +0100

    Changed the pressure project
    Now has a customisable "spread" and the colours are nicer! Also fixed a crash when drawing outsize valid area. :smile:!

--- projects/pressure/sketch.js
+++ projects/pressure/sketch.js
@@ -1,32 +1,41 @@
 function setup() {
     createCanvas(600,600)
 
-    gridSize = 20
+    spread = 40
+    createP("Spread:")
+    spreadSlider = createSlider(0,45, 10).size(600)
+    gridSize = 12
     gridW = width/gridSize
     gridH = height/gridSize
     grid = []
     for (x=0; x<gridW; x++) {
         grid.push([])
         for (y=0; y<gridH; y++) {
-            grid[x].push(0)
+            grid[x].push( random(0,255) )
         }
     }
 }
 
 function draw() {
+    spread = 50-spreadSlider.value()
+
     if (mouseIsPressed) {
         x = floor(mouseX/gridSize)
         y = floor(mouseY/gridSize)
-        if (mouseButton == LEFT) {
-            grid[x][y] += 1000
-        } else if (mouseButton == RIGHT) {
-            grid[x][y] -= 1000
-        }
-        if (grid[x][y] > 255) {
-            grid[x][y] = 255
-        }
-        if (grid[x][y] < 0) {
-            grid[x][y] = 0
+        if (x < 0 || y < 0 || x > gridW-1 || y > gridW-1) {
+            console.log("Invalid pos")
+        } else {
+            if (mouseButton == LEFT) {
+                grid[x][y] += 1500
+            } else if (mouseButton == RIGHT) {
+                grid[x][y] -= 1500
+            }
+            if (grid[x][y] > 255) {
+                grid[x][y] = 255
+            }
+            if (grid[x][y] < 0) {
+                grid[x][y] = 0
+            }
         }
     }
 
@@ -36,7 +45,7 @@ function draw() {
     noStroke()
     for (x=0; x<gridW; x++) {
         for (y=0; y<gridH; y++) {
-            temp = grid[x][y]/5
+            temp = grid[x][y]/spread
             if (x > 0) {
                 grid[x-1][y] += temp
                 grid[x][y] -= temp
@@ -53,11 +62,12 @@ function draw() {
                 grid[x][y+1] += temp
                 grid[x][y] -= temp
             }
-            push()
-            colorMode(HSB, 255)
-            fill(grid[x][y], 200, grid[x][y])
+            // Sky is (supposedly) 52.9% red, 80.8% green and 92.2% blue
+            r = grid[x][y] * 0.529
+            g = grid[x][y] * 0.808
+            b = grid[x][y] * 0.922
+            fill(r,g,b)
             rect(x*gridSize,y*gridSize, gridSize,gridSize)
-            pop()
         }
     }
 }
