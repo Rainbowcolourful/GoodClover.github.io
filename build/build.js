f35ca65d29106749fe3edab6a25eb3c99fa29b19
GoodClover <oliversimmo@gmail.com>
2020-04-27 18:39:06 +0100

    Added all those features with meta and stuff.

--- build/build.js
+++ build/build.js
@@ -1,37 +1,58 @@
 // Get the modules
 const marked = require("marked");
 const fs = require("fs");
+const yaml = require("js-yaml");
 
+// Load the build data
 const build_data = require("./build_data.json");
 
 // Set options, see https://marked.js.org/#/USING_ADVANCED.md
 marked.setOptions({
     ...build_data.options,
-    //sanitize: false, // DEPRECATED
+    // sanitize: false, // DEPRECATED
     renderer: new marked.Renderer() // See https://marked.js.org/#/USING_PRO.md
 });
 
 // Read template
-var template = fs.readFileSync("./template.html", "utf8")
+var template = fs.readFileSync("./template.html", "utf8");
 
 // Compile the code
 for (var conversion of build_data.pages) {
-    fs.readFile("../"+conversion.markdown, "utf8", (err, data) => {
+    // Load the file
+    var markdown  = fs.readFileSync(`../${conversion}.md`, "utf8");
+
+    // Load the metadata
+    var meta =  yaml.safeLoad(markdown.slice(4, markdown.slice(4).indexOf("---")+3));
+    meta = {...build_data.default_meta, ...meta};
+
+    // Remove metadata from markdown
+    markdown = markdown.slice(markdown.slice(4).indexOf("---")+7);
+
+    // Replace metadata into markdown
+    for (var i = 0; i<markdown.length; i++) {
+        var sly = markdown.slice(i);
+        if (sly.slice(0, 9) == "[_meta_: ") {
+            markdown = markdown.slice(0, i) + meta[sly.slice(9, sly.indexOf("]"))] + sly.slice(sly.indexOf("]")+1)
+        };
+    };
+
+    // Convert the markdown into html
+    var html = marked(markdown);
+
+    // Add template data
+    html = template.replace("META_BODY", html);
+    html = html.replace("META_TITLE", meta.Title);
+    html = html.replace("META_CSS", `./css/${meta.Style}.css`);
+    for (var i in meta) {
+        if (meta[i].replace(" ", "") != "") {
+            html = html.replace("METAS", `<meta name="${i.toLowerCase()}" content="${meta[i]}"/>\n        METAS`);
+        };
+    };
+    html = html.replace("\n        METAS", "");
+
+    // Write the html to a file
+    fs.writeFile(`../${conversion}.html`, html, (err) => {
         if (err) throw err;
-
-        // Convert the markdown into html
-        data = marked(data);
-
-        // Add tamplate data
-        data = template.replace("DATA_BODY", data);
-        data = data.replace("DATA_TITLE", conversion.title);
-        data = data.replace("DATA_CSS", conversion.css);
-
-        // Write it to a file
-        fs.writeFile("../"+conversion.html, data, (err) => {
-            if (err) throw err;
-            console.log(`${conversion.markdown} => ${conversion.html}`);
-        });
-
+        console.log(`${conversion}.md => ${conversion}.html`);
     });
 };
--- index.md
+++ index.md
@@ -1,8 +1,28 @@
-Oliver Simmons' (GoodClover's) Page
-===================================
+---
+Title: Oliver Simmons' Page
+Author: Oliver Simmons (GoodClover)
+Style: GitHub
+---
+
+# Oliver Simmons' (GoodClover's) Page
+
+Name | Age | Gender
+---: | --- | :-----
+ Bob | 13  | Male
+Mary | 23  | Female
+Jhon | 52  | Male
 
 I make random little projects, that usually don't get very far.
 
+Here is a meta/special info test for my custom markdown generator I made to create this site:
 
+1. Made by [_meta_: Author].
+2. Titled [_meta_: Title].
+3. Built/converted on [_build_: Date].
+4. With the CSS style of [_meta_: Style].
 
-[Old Site](./OldSite/index.html)
+![image](./images/GoogleLogoNew.png)
+
+I have some JavaScript projects made with p5.js on the [Old Site] if you want to take a look.
+
+[old site]: ./OldSite/index.html
